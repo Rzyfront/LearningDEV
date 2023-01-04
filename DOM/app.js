e98ce4591fb3e8b43a3d55f4f58a357dd3c859dc
Rzyfront <rzyfront@gmail.com>
2023-01-03 23:05:22 -0500

    DOM

--- DOM/app.js
+++ DOM/app.js
@@ -32,6 +32,53 @@ console.log(document.getElementById("parrafo2"));
 //querySelectorAll() selecciona todos los elementos que coincidan con el parametro
 console.log(document.querySelectorAll(".all"));
 
-
+//Podemos guardar el elemento html seleccionado en una variable y utilizarla
 const parrafo1= document.getElementById("parrafo1");
-parrafo1.textContent="Texto desde JavaScrip"
+//textContent modifica el contenido del texto de un elemento.
+parrafo1.textContent="Texto desde JavaScrip";
+
+const parrafo2 = document.getElementById("parrafo2");
+//innetHTML modifica el contenido de un texto pero conservando el uso de Etiquetas
+parrafo2.innerHTML="<b>texto Inner HTML</b>";
+
+parrafo1.classList.add("all");
+
+
+//Crear un elemto en la lista
+const lista= document.getElementById("lista");
+
+const li= document.createElement("li");
+li.textContent="Primer Elemto";
+//Agrega la etiqueta dentro de la etiqueta seleccionada
+lista.appendChild(li);
+
+//Crear varios elemnto usando arrays
+const Arrayelement= ["Segundo Elemento", "Tercer Elemento"];
+
+Arrayelement.forEach(item => {
+    const liarray= document.createElement("li");
+    liarray.textContent=item;
+    lista.appendChild(liarray);
+});
+
+//Crear varios elemtos y agregarlos a la lista con InnerHTML
+const listainner= document.getElementById("innerlist");
+const ArrayInner= ["Primer Inner", "Segundo Inner", "Tercer Inner"];
+
+ArrayInner.forEach(Elemt=>{
+    listainner.innerHTML+= `<li>${Elemt}</li>`;
+});
+
+
+
+const listafragmentada= document.getElementById("frangmentlist");
+const Fragment = document.createDocumentFragment();
+const Arrayfragment= ["Fragment 1","Fragment 2","Fragment 3"];
+
+Arrayfragment.forEach(Obj=>{
+    const lifrag= document.createElement("li");
+    lifrag.textContent= Obj;
+    Fragment.appendChild(lifrag);
+})
+
+listafragmentada.appendChild(Fragment);
